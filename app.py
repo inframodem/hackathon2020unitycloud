dd6cf70793a336c6199c12b540a56b773b976644
Cody Rhee <crhee94@uw.edu>
2020-04-18 14:23:51 -0700

    adding configuration

--- app.py
+++ app.py
@@ -2,40 +2,19 @@ from flask import Flask, render_template, request
 #import firebase_admin
 #from firebase_admin import credentials, firestore
 from firebase import firebase
+import configparser
 
 app = Flask(__name__)
 
+config = configparser.ConfigParser()
+config.read('configs/config.ini')
+
 # REALTIME DB; need to pass in firebaseio address as an argument in FirebaseApplication()
-# firebase = firebase.FirebaseApplication()   
+firebase = firebase.FirebaseApplication(config['firebase']['address'])   
 
-""" USING REALTIME DB NOSQL
-data = {
-    'Name': 'Hello There',
-    'Score': 5
-}
-
-result = firebase.post('', data)    # Need to pass in db in first argument
+#USING REALTIME DB NOSQL
+result = firebase.get(config['firebase']['database'], '')
 print(result)
-"""
-
-""" USING FIRESTORE NOSQL
-cred = credentials.Certificate()    # Pass in service account json key obtained from GCP
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-doc_ref = db.collection(u'users').document(u'test')
-doc_ref.set({
-    u'name': u'test name',
-    u'score': 0
-})
-
-users_ref = db.collection(u'users')
-docs = users_ref.stream()
-
-for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
-"""
-
 
 @app.route("/")
 def home():
